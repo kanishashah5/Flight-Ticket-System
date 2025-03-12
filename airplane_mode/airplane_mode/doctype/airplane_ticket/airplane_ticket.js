5921b6a7a4d38a6a5c5408edde57006ca449b46d
Kanisha Shah <kanishah0505@gmail.com>
2025-03-12 18:51:16 +0530

    Day-3

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,37 +1,6 @@
 // // Copyright (c) 2025, A and contributors
 // // For license information, please see license.txt
 
-// // frappe.ui.form.on("Airplane Ticket", {
-// // 	refresh(frm) {
-
-// // 	},
-// // });
-// // frappe.ui.form.on('Airplane Ticket', {
-// //     refresh: function(frm) {
-// //         // Add a custom button
-// //         frm.add_custom_button('Assign Seat', function() {
-// //             // Create a dialog to enter seat number
-// //             let d = new frappe.ui.Dialog({
-// //                 title: 'Enter Seat Number',
-// //                 fields: [
-// //                     {
-// //                         label: 'Seat Number',
-// //                         fieldname: 'seat_number',
-// //                         fieldtype: 'Data',
-// //                         reqd: 1
-// //                     }
-// //                 ],
-// //                 primary_action_label: 'Set Seat',
-// //                 primary_action(values) {
-// //                     frm.set_value('seat', values.seat_number);  // Set seat number in the form
-// //                     d.hide();
-// //                 }
-// //             });
-
-// //             d.show();
-// //         });
-// //     }
-// // });
 
 frappe.ui.form.on('Airplane Ticket', {
     refresh: function(frm) {
@@ -52,12 +21,3 @@ frappe.ui.form.on('Airplane Ticket', {
         }
     }
 });
-
-
-
-// Copyright (c) 2025, Sanskar and contributors
-// For license information, please see license.txt
-
-~
-
-
--- airplane_mode/airplane_mode/python/airplane_ticket.py
+++ airplane_mode/airplane_mode/python/airplane_ticket.py
@@ -20,8 +20,8 @@ def before_save(doc, method=None):
                 doc.seat = seat
                 break
 
-def get_existing_seats():
-    seats = frappe.get_all("Airplane Ticket", filters={}, fields=["seat"])
+def get_existing_seats(flight):
+    seats = frappe.get_all("Airplane Ticket", filters={"flight": flight}, fields=["seat"])
     return {s["seat"] for s in seats if s["seat"]}
 
 
@@ -35,7 +35,37 @@ def validate(doc, method=None):
             unique_addons[addon.item] = addon
             filtered_addons.append(addon)
 
-    doc.set("add_ons", filtered_addons)
+        # Get the airplane linked to the flight
+    airplane = frappe.db.get_value("Airplane Flight", doc.flight, "airplane")
+
+    if not airplane:
+        frappe.throw("The selected flight does not have a linked airplane.")
+
+    # Get the airplane's seat capacity
+    capacity = frappe.db.get_value("Airplane", airplane, "capacity")
+
+    if not capacity:
+        frappe.throw(f"The airplane {airplane} does not have a defined capacity.")
+
+    # Count existing tickets for this flight, including the one being saved
+    booked_tickets = frappe.db.count("Airplane Ticket", {"flight": doc.flight}) + 1  # Add 1 for current doc
+
+    # Ensure capacity is not exceeded
+    if booked_tickets > capacity:
+        frappe.throw(f"No available seats! The airplane capacity ({capacity}) has been reached.")
+
+    # Assign a unique seat if not already assigned
+    if not doc.seat:
+        existing_seats = get_existing_seats(doc.flight)
+
+        while True:
+            row = booked_tickets  # Sequentially assign seat row
+            seat_letter = ["A", "B", "C", "D", "E", "F"][booked_tickets % 6]  # Rotate seat letters
+            seat = f"{row}{seat_letter}"
+
+            if seat not in existing_seats:
+                doc.seat = seat
+                break
 
 
 def prevent_submission(doc, method):
